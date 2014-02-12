d6d4c561275838d0c860306f9efb418f7e688511
Samiullah Khan Suryazi <git@reseller.suryazi.com>
2014-02-12 06:58:14 +0300

    def random() added

--- grails-app/controllers/qotd/QuoteController.groovy
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -2,8 +2,15 @@ package qotd
 
 class QuoteController {
 
-    def index() { }
+    def index() {
+        redirect (action:home)
+    }
     def home(){
         render"<h1>Real programmers do not eat Quiche</h1>"
+    }
+    def random={
+        def staticAuthor="anonymous"
+        def staticContent = "Real Programmers don't eat quiche"
+        [author:staticAuthor, content:staticContent]
     }
 }
